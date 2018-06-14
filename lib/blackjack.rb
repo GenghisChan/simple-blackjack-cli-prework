3f8835baed5cd52ccb8090e16c679641ac0a92db
Christofer Chan <devchumbing@gmail.com>
2018-06-14 13:38:42 +0000

    automatically backed up by learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -52,5 +52,12 @@ end
 #####################################################
 
 def runner
-  # code runner here
+  
+  welcome
+  initial_round
+  until > 21
+  hit?
+  display_card_total
+  end_game
+  
 end
